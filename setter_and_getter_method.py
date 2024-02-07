f72270ea526843378f75b58d00e0f0d6dd543a5f
Nitay_Das <nitaydas910@gmail.com>
2024-02-07 15:12:02 +0600

    adding comment

--- setter_and_getter_method.py
+++ setter_and_getter_method.py
@@ -4,17 +4,23 @@ class Login:
         self.password = password
         
     def set_new_pass(self,new_password):
+        #if password is not same as older password
         if(self.password != new_password):
             self.password = new_password
+        #if password is same as older password
         else:
             print(f"Password shoudn't be same as older password !")
             
+    #return user info      
     def get_user_info(self):
         return f"Username: {self.usename}, Password: {self.password}\n"
     
+#creating object   
 user = Login("nitay","190129")
+#get user info
 print(user.get_user_info())
 
+#set new pass
 user.set_new_pass("190129")
 print(user.get_user_info())
 
